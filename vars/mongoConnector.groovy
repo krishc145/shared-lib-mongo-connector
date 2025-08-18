6c9ef19f147af1358512e72d9e73f6f777516b5c
krishc145 <krishnakumarchinnusamy@gmail.com>
2025-08-18 07:05:27 +0530

    added latest changes

--- vars/mongoConnector.groovy
+++ vars/mongoConnector.groovy
@@ -1,12 +1,32 @@
-// mongoConnector.groovy
-@GrabResolver(name='localLib', root='file://C:/path/to/shared-lib-mongo-connector/lib/')
-@Grab('org.mongodb:mongodb-driver-sync:4.11.5')
+// File: mongoConnector.groovy
+// No @Grab needed since JAR is inside lib/ of shared library
 
-import com.mongodb.client.MongoClients
+package com.yourcompany.mongo
+
 import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
 
-def connect(String uri = "mongodb://localhost:27017") {
-    echo "Connecting to MongoDB at ${uri}"
-    MongoClient client = MongoClients.create(uri)
-    return client
+class mongoConnector implements Serializable {
+
+    /**
+     * Connects to MongoDB.
+     * @param uri MongoDB connection URI (default: "mongodb://localhost:27017")
+     * @return MongoClient instance
+     */
+    static MongoClient connect(String uri = "mongodb://localhost:27017") {
+        println "Connecting to MongoDB at ${uri}"
+        MongoClient client = MongoClients.create(uri)
+        return client
+    }
+
+    /**
+     * Closes the given MongoClient connection.
+     * @param client MongoClient instance to close
+     */
+    static void close(MongoClient client) {
+        if (client != null) {
+            client.close()
+            println "MongoDB connection closed"
+        }
+    }
 }
--- vars/mongoOperations.groovy
+++ vars/mongoOperations.groovy
@@ -1,22 +1,63 @@
+// File: mongoOperations.groovy
+// No @Grab needed since JAR is inside lib/ of shared library
+
+package com.yourcompany.mongo
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoDatabase
+import com.mongodb.client.MongoCollection
 import org.bson.Document
+import com.mongodb.MongoWriteException
 
-def createDatabase(mongoClient, String dbName) {
-    def db = mongoClient.getDatabase(dbName)
-    echo "Database '${dbName}' is ready"
-    return db
-}
+class mongoOperations implements Serializable {
 
-def createCollection(db, String collectionName) {
-    try {
-        db.createCollection(collectionName)
-        echo "Collection '${collectionName}' created"
-    } catch (Exception e) {
-        echo "Collection '${collectionName}' may already exist: ${e.message}"
+    /**
+     * Create or get a database
+     * @param client MongoClient instance
+     * @param dbName Database name
+     * @return MongoDatabase instance
+     */
+    static MongoDatabase createDatabase(MongoClient client, String dbName) {
+        println "Database '${dbName}' is ready"
+        return client.getDatabase(dbName)
     }
-}
 
-def insertDocument(db, String collectionName, Map doc) {
-    def collection = db.getCollection(collectionName)
-    collection.insertOne(new Document(doc))
-    echo "Inserted document into '${collectionName}' -> ${doc}"
+    /**
+     * Create a collection if it does not exist
+     * @param db MongoDatabase instance
+     * @param collectionName Name of the collection
+     */
+    static void createCollection(MongoDatabase db, String collectionName) {
+        try {
+            if (!db.listCollectionNames().into([]).contains(collectionName)) {
+                db.createCollection(collectionName)
+                println "Collection '${collectionName}' created"
+            } else {
+                println "Collection '${collectionName}' already exists"
+            }
+        } catch (MongoWriteException e) {
+            println "Write exception while creating collection: ${e.error.code} - ${e.error.message}"
+        } catch (Exception e) {
+            println "Error creating collection: ${e.message}"
+        }
+    }
+
+    /**
+     * Insert a document into a collection
+     * @param db MongoDatabase instance
+     * @param collectionName Name of the collection
+     * @param data Map containing document data
+     */
+    static void insertDocument(MongoDatabase db, String collectionName, Map data) {
+        MongoCollection<Document> collection = db.getCollection(collectionName)
+        try {
+            collection.insertOne(new Document(data))
+            println "Document inserted into '${collectionName}'"
+        } catch (MongoWriteException e) {
+            println "Write exception: ${e.error.code} - ${e.error.message}"
+        } catch (Exception e) {
+            println "Error inserting document: ${e.message}"
+        }
+    }
+
 }
