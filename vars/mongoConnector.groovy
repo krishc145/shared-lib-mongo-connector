e565d8a538498eeae1fd1eb761f48069cc71da33
krishc145 <krishnakumarchinnusamy@gmail.com>
2025-08-18 07:52:15 +0530

    files

--- vars/mongoConnector.groovy
+++ vars/mongoConnector.groovy
@@ -1,32 +1,6 @@
-// File: mongoConnector.groovy
-// No @Grab needed since JAR is inside lib/ of shared library
-
-package com.yourcompany.mongo
-
-import com.mongodb.client.MongoClient
-import com.mongodb.client.MongoClients
-
-class mongoConnector implements Serializable {
-
-    /**
-     * Connects to MongoDB.
-     * @param uri MongoDB connection URI (default: "mongodb://localhost:27017")
-     * @return MongoClient instance
-     */
-    static MongoClient connect(String uri = "mongodb://localhost:27017") {
-        println "Connecting to MongoDB at ${uri}"
-        MongoClient client = MongoClients.create(uri)
-        return client
-    }
-
-    /**
-     * Closes the given MongoClient connection.
-     * @param client MongoClient instance to close
-     */
-    static void close(MongoClient client) {
-        if (client != null) {
-            client.close()
-            println "MongoDB connection closed"
-        }
-    }
+def call() {
+    // Adjust path and classpath as needed
+    bat """
+    groovy -cp "K:\\...\\lib\\mongodb-driver-sync-5.5.0.jar" K:\\...\\mongoConnector.groovy
+    """
 }
--- vars/mongoOperations.groovy
+++ vars/mongoOperations.groovy
@@ -1,6 +1,3 @@
-// File: mongoOperations.groovy
-// No @Grab needed since JAR is inside lib/ of shared library
-
 package com.yourcompany.mongo
 
 import com.mongodb.client.MongoClient
@@ -9,24 +6,13 @@ import com.mongodb.client.MongoCollection
 import org.bson.Document
 import com.mongodb.MongoWriteException
 
-class mongoOperations implements Serializable {
+class MongoOperations implements Serializable {
 
-    /**
-     * Create or get a database
-     * @param client MongoClient instance
-     * @param dbName Database name
-     * @return MongoDatabase instance
-     */
     static MongoDatabase createDatabase(MongoClient client, String dbName) {
         println "Database '${dbName}' is ready"
         return client.getDatabase(dbName)
     }
 
-    /**
-     * Create a collection if it does not exist
-     * @param db MongoDatabase instance
-     * @param collectionName Name of the collection
-     */
     static void createCollection(MongoDatabase db, String collectionName) {
         try {
             if (!db.listCollectionNames().into([]).contains(collectionName)) {
@@ -36,18 +22,12 @@ class mongoOperations implements Serializable {
                 println "Collection '${collectionName}' already exists"
             }
         } catch (MongoWriteException e) {
-            println "Write exception while creating collection: ${e.error.code} - ${e.error.message}"
+            println "Write exception: ${e.error.code} - ${e.error.message}"
         } catch (Exception e) {
             println "Error creating collection: ${e.message}"
         }
     }
 
-    /**
-     * Insert a document into a collection
-     * @param db MongoDatabase instance
-     * @param collectionName Name of the collection
-     * @param data Map containing document data
-     */
     static void insertDocument(MongoDatabase db, String collectionName, Map data) {
         MongoCollection<Document> collection = db.getCollection(collectionName)
         try {
@@ -59,5 +39,4 @@ class mongoOperations implements Serializable {
             println "Error inserting document: ${e.message}"
         }
     }
-
 }
